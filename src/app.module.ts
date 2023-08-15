666d656dcbf28fac68683cf4987c8f316585591c
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-15 18:49:42 +0300

    mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://maksympaselsky:a4x5KvI3hEUDmvP6@testtasks.4dy901l.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
