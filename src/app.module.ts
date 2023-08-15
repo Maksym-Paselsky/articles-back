bccd38929a3d80b90e926839be90a0bb8a45e607
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-15 18:54:03 +0300

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://maksympaselsky:a4x5KvI3hEUDmvP6@testtasks.4dy901l.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
