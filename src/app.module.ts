2f51d12a293ea4a1e62e0137a0ddd9513d160550
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-16 09:58:32 +0300

    axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksService } from './cron/tasks.service';
+import { HttpModule } from '@nestjs/axios';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 3,
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     ScheduleModule.forRoot(),
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService, TasksService],
--- src/cron/tasks.service.ts
+++ src/cron/tasks.service.ts
@@ -1,10 +1,22 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TasksService {
+  constructor(private readonly httpService: HttpService) {}
+
   private readonly logger = new Logger(TasksService.name);
 
   @Cron(CronExpression.EVERY_5_SECONDS)
-  handleCron() {}
+  importArticles() {
+    this.logger.debug('Importing articles');
+    this.httpService.axiosRef
+      .get(
+        `http://api.mediastack.com/v1/news?access_key=${process.env.MEDIASTACK_API_KEY}&country=ua&keywords=ukraine russia war`,
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 }
