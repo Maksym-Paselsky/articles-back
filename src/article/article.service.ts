f7b577d02a4fa33309b28a55c1b6320a5bfa12e2
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-16 10:50:24 +0300

    crud

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Article } from './entities/article.entity';
+import { Logger } from '@nestjs/common';
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+  constructor(
+    @InjectModel('Article') private readonly articleModel: Model<Article>,
+  ) {}
+
+  private readonly logger = new Logger(ArticleService.name);
+
+  async create(createArticleDto: CreateArticleDto): Promise<Article> {
+    const createdArticle = new this.articleModel(createArticleDto);
+    return await createdArticle.save();
   }
 
-  findAll() {
-    return `This action returns all article`;
+  async findAll(): Promise<Article[]> {
+    return await this.articleModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async findOne(id: string) {
+    try {
+      return await this.articleModel.findById(id).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find article.');
+    }
   }
 
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+  async update(id: string, updateArticleDto: UpdateArticleDto) {
+    try {
+      return await this.articleModel
+        .findByIdAndUpdate(id, updateArticleDto, { new: true })
+        .exec();
+    } catch (error) {
+      this.logger.error(error);
+      throw new NotFoundException('Could not update the article.');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} article`;
+  async remove(id: string) {
+    return await this.articleModel.findByIdAndRemove(id).exec();
   }
 }
