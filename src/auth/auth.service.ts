d22aeb7ec077b1bdfc8e71b6c95e4a0f0bb74ddc
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-16 18:08:33 +0300

    add bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,6 @@ import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
-const saltOrRounds = 10;
-const password = 'random_password';
-const hash = await bcrypt.hash(password, saltOrRounds);
 @Injectable()
 export class AuthService {
   constructor(
@@ -15,13 +12,12 @@ export class AuthService {
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
-    const saltOrRounds = 10;
-    const password = 'admin';
-    const hash = await bcrypt.hash(password, saltOrRounds);
 
-    if (user?.password !== pass) {
+    // check if the password is valid
+    if ((await bcrypt.compare(pass, user.password)) === false) {
       throw new UnauthorizedException();
     }
+
     const payload = { sub: user.id, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
